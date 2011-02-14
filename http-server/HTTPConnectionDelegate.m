ddb5b33725a9f265862df45de7da73184a17f85f
Mirko Kiefer <mail@mirkokiefer.com>
2011-02-14 16:58:04 +0100

    basic structure of HTTPConnectionDelegate

--- http-server/HTTPConnectionDelegate.m
+++ http-server/HTTPConnectionDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "HTTPConnectionDelegate.h"
-
+#import <CoreFoundation/CoreFoundation.h>
 
 @implementation HTTPConnectionDelegate
 
@@ -25,10 +25,33 @@
 {
     [super dealloc];
 }
--(void)HTTPServer:(HTTPServer *)server didMakeNewConnection:(HTTPConnection *)connection {
+- (void)HTTPServer:(HTTPServer *)server didMakeNewConnection:(HTTPConnection *)connection {
   
 }
 - (void)HTTPConnection:(HTTPConnection *)connection didReceiveRequest:(HTTPServerRequest *)request {
+  CFHTTPMessageRef message = [request request];
+  NSString* method = [(NSString*) CFHTTPMessageCopyRequestMethod(message) autorelease];
+  NSURL* url = [(NSURL*) CFHTTPMessageCopyRequestURL(message) autorelease];
   
+  if ([method isEqual:@"GET"]) {
+    // Get clipboard item at URL
+    NSURL *itemURL;
+    
+    NSData *itemData = [NSData dataWithContentsOfURL:itemURL];
+    CFHTTPMessageRef response = CFHTTPMessageCreateResponse(kCFAllocatorDefault, 200, NULL, kCFHTTPVersion1_1); // OK
+    CFHTTPMessageSetHeaderFieldValue(response, (CFStringRef)@"Content-Length", (CFStringRef)[NSString stringWithFormat:@"%d", [itemData length]]);
+    [request setResponse:response];
+    CFRelease(response);
+  }
+  if ([method isEqual:@"POST"]) {
+    NSData* itemData = (NSData*) CFHTTPMessageCopyBody(message);
+    /* Set clipboard item at URL
+     
+    */
+    CFHTTPMessageRef response = CFHTTPMessageCreateResponse(kCFAllocatorDefault, 200, NULL, kCFHTTPVersion1_1); // OK
+    [request setResponse:response];
+    CFRelease(response);
+  }
+  return;
 }
 @end
