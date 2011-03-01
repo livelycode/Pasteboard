fc5f9f0e808918b4b25f3c0a3b21d72c81f0cc33
Mirko Kiefer <mail@mirkokiefer.com>
2011-03-01 16:41:29 +0100

    http server not common

---
+++ iOS/http-server/HTTPServer.h
@@ -0,0 +1,112 @@
+/*
+ File: HTTPServer.h
+ 
+ Abstract: Interface description for a basic HTTP server Foundation class
+ */ 
+
+#import "TCPServer.h"
+
+@class HTTPConnection, HTTPServerRequest;
+
+@interface HTTPServer : TCPServer {
+@private
+    Class connClass;
+    NSURL *docRoot;
+}
+
+- (Class)connectionClass;
+- (void)setConnectionClass:(Class)value;
+// used to configure the subclass of HTTPConnection	 to create when  
+// a new connection comes in; by default, this is HTTPConnection
+
+- (NSURL *)documentRoot;
+- (void)setDocumentRoot:(NSURL *)value;
+
+@end
+
+@interface HTTPServer (HTTPServerDelegateMethods)
+- (void)HTTPServer:(HTTPServer *)serv didMakeNewConnection:(HTTPConnection *)conn;
+// If the delegate implements this method, this is called  
+// by an HTTPServer when a new connection comes in.  If the
+// delegate wishes to refuse the connection, then it should
+// invalidate the connection object from within this method.
+@end
+
+
+// This class represents each incoming client connection.
+@interface HTTPConnection : NSObject {
+@private
+    id delegate;
+    NSData *peerAddress;
+    HTTPServer *server;
+    NSMutableArray *requests;
+    NSInputStream *istream;
+    NSOutputStream *ostream;
+    NSMutableData *ibuffer;
+    NSMutableData *obuffer;
+    BOOL isValid;
+    BOOL firstResponseDone;
+}
+
+- (id)initWithPeerAddress:(NSData *)addr inputStream:(NSInputStream *)istr outputStream:(NSOutputStream *)ostr forServer:(HTTPServer *)serv;
+
+- (id)delegate;
+- (void)setDelegate:(id)value;
+
+- (NSData *)peerAddress;
+
+- (HTTPServer *)server;
+
+- (HTTPServerRequest *)nextRequest;
+// get the next request that needs to be responded to
+
+- (BOOL)isValid;
+- (void)invalidate;
+// shut down the connection
+
+- (void)performDefaultRequestHandling:(HTTPServerRequest *)sreq;
+// perform the default handling action: GET and HEAD requests for files
+// in the local file system (relative to the documentRoot of the server)
+
+@end
+
+@interface HTTPConnection (HTTPConnectionDelegateMethods)
+- (void)HTTPConnection:(HTTPConnection *)conn didReceiveRequest:(HTTPServerRequest *)mess;
+- (void)HTTPConnection:(HTTPConnection *)conn didSendResponse:(HTTPServerRequest *)mess;
+// The "didReceiveRequest:" is the most interesting -- 
+// tells the delegate when a new request comes in.
+@end
+
+
+// As NSURLRequest and NSURLResponse are not entirely suitable for use from 
+// the point of view of an HTTP server, we use CFHTTPMessageRef to encapsulate
+// requests and responses.  This class packages the (future) response with a
+// request and other info for convenience.
+@interface HTTPServerRequest : NSObject {
+@private
+    HTTPConnection *connection;
+    CFHTTPMessageRef request;
+    CFHTTPMessageRef response;
+    NSInputStream *responseStream;
+}
+
+- (id)initWithRequest:(CFHTTPMessageRef)req connection:(HTTPConnection *)conn;
+
+- (HTTPConnection *)connection;
+
+- (CFHTTPMessageRef)request;
+
+- (CFHTTPMessageRef)response;
+- (void)setResponse:(CFHTTPMessageRef)value;
+// The response may include a body.  As soon as the response is set, 
+// the response may be written out to the network.
+
+- (NSInputStream *)responseBodyStream;
+- (void)setResponseBodyStream:(NSInputStream *)value;
+// If there is to be a response body stream (when, say, a big
+// file is to be returned, rather than reading the whole thing
+// into memory), then it must be set on the request BEFORE the
+// response [headers] itself.
+
+@end
+
