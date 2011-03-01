c4619f42004bae901cd5c09753216a5e4a496ece
Mirko Kiefer <mail@mirkokiefer.com>
2011-03-01 16:57:16 +0100

    ClipboardController not common anymore

---
+++ iOS/CBClipboardController.h
@@ -0,0 +1,29 @@
+#import "Cocoa.h"
+#import "CBClipboardViewDelegate.h"
+
+@class CBClipboard;
+@class CBClipboardView;
+
+@interface CBClipboardController : NSObject
+{
+@private
+  CBClipboard *clipboard;
+  CBClipboardView *clipboardView;
+  id changeListener;
+}
+
+- (id)initWithFrame:(CGRect)aFrame
+     viewController:(id)viewController;
+
+- (void)insertItem:(CBItem *)newItem
+           atIndex:(NSInteger)anIndex;
+
+- (BOOL)clipboardContainsItem:(CBItem *)anItem;
+
+- (void)addChangeListener:(id)anObject;
+
+@end
+
+@interface CBClipboardController(Delegation) <CBClipboardViewDelegate>
+
+@end
---
+++ iOS/CBClipboardController.m
@@ -0,0 +1,77 @@
+#import "Cloudboard.h"
+
+@implementation CBClipboardController
+
+- (id)initWithFrame:(CGRect)aFrame
+     viewController:(id)viewController;
+{
+  self = [super init];
+  if (self != nil)
+  {
+    clipboard = [[CBClipboard alloc] initWithCapacity:8];
+    
+    clipboardView = [[CBClipboardView alloc] initWithFrame:aFrame
+                                                   padding:20
+                                                      Rows:4
+                                                   Columns:2];
+    [clipboardView setDelegate:self];
+    [viewController addSubview:clipboardView];
+  }
+  return self;
+}
+
+- (void)insertItem:(CBItem *)newItem
+           atIndex:(NSInteger)anIndex
+{
+  NSUInteger newIndex = anIndex - [clipboardView invisibleItemsUpToIndex:anIndex];
+  [clipboard insertItem:newItem
+                atIndex:newIndex];
+  
+  NSUInteger indexMove = [clipboardView itemViews] - 1;
+  while (indexMove != anIndex)
+  {
+    NSUInteger previousIndex = indexMove - 1;
+    BOOL previousVisible = [clipboardView itemAtIndexIsVisible:previousIndex];
+    NSAttributedString *previousString = [clipboardView stringForItemAtIndex:previousIndex];
+    [clipboardView setVisible:previousVisible
+               forItemAtIndex:indexMove];
+    [clipboardView setString:previousString
+              forItemAtIndex:indexMove];
+    indexMove = indexMove - 1;
+  }
+  
+  NSAttributedString *string = [newItem string];
+  [clipboardView setVisible:YES
+             forItemAtIndex:anIndex];
+  [clipboardView setString:string
+            forItemAtIndex:newIndex];
+  
+  if (changeListener != nil)
+  {
+    [changeListener insertedItem:newItem
+                         atIndex:anIndex];
+  }
+}
+
+- (BOOL)clipboardContainsItem:(CBItem *)anItem
+{
+  return [[clipboard items] containsObject:anItem];
+}
+
+- (void)addChangeListener:(id)anObject
+{
+  changeListener = [anObject retain];
+}
+
+- (void)dealloc {
+  [clipboard release];
+  [clipboardView release];
+  [changeListener release];
+  [super dealloc];
+}
+
+@end
+
+@implementation CBClipboardController(Delegation)
+
+@end
