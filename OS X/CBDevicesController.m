8857101e9f58dd1c8f54d91a63ff1af8d6b648d0
Johannes Auer <johannes.auer@me.com>
2011-03-02 14:09:25 +0100

    tableviews added to the devices pane

--- OS X/CBDevicesController.m
+++ OS X/CBDevicesController.m
@@ -2,14 +2,32 @@
 
 @implementation CBDevicesController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Initialization code here.
-    }
-    
-    return self;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    foundClipboards = [NSMutableArray array];
+    [foundClipboards addObject:@"foo"];
+  }
+  return self;
+}
+
+@end
+
+@implementation CBDevicesController(Delegation)
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView {
+  return [foundClipboards count];
+}
+
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex {
+  id object = nil;
+  if (aTableView == foundClipboardsView) {
+    object = @"foo";
+  }
+  if (aTableView == registeredClipboardsView) {
+    object = @"bar";
+  }
+  return object;
 }
 
 @end
