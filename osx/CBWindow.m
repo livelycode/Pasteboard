8b861097d2df172f6225f1282a4c98100cb09f9b
Johannes Auer <johannesauer@macbookpro.fritz.box>
2011-04-16 18:44:37 +0200

    pasteboard is frontmost window again

--- osx/CBWindow.m
+++ osx/CBWindow.m
@@ -6,7 +6,7 @@
     self = [super initWithContentRect:aRect styleMask:NSBorderlessWindowMask backing:NSBackingStoreBuffered defer:NO];
     if (self != nil) {
       [self setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces];
-    //  [self setLevel:NSStatusWindowLevel];
+      [self setLevel:NSStatusWindowLevel];
       [self setOpaque:NO];
       [self setBackgroundColor:[NSColor clearColor]];
     }
